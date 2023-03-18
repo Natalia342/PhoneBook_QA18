39aa46ddaa9248f742a4c3325305b2c2e2f726bd
Natalia342 <nataliatalova7@gmail.com>
2023-03-18 17:11:41 +0200

    constructor

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,20 +1,22 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
-//    @BeforeMethod
-//    public void preCondition(){
-//        if(isLogged()){
-//            logout();
-//        }
-//    }
+    @BeforeMethod
+    public void preCondition(){
+        if(app.getUser().isLogged()){
+            app.getUser().logout();
+        }
+   }
 @Test
     public void loginPositive(){
-    app.getUser().openLoginRegistration();
+
     String email ="vasiatal@gmail.com";
     String password ="Vasia$1234";
+    app.getUser().openLoginRegistration();
     app.getUser().fillLoginRegistrationForm(email,password);
     app.getUser().submitLogin();
  //   Assert.assertTrue(wd.findElement(By.xpath("//a[text()='ADD']")).getText().equals("ADD"));
@@ -31,11 +33,12 @@ public class LoginTests extends TestBase {
 
 //    Assert.assertTrue(wd.findElement(By.xpath("//div[.='Login Failed with code 401']")).getText().equals("Login Failed with code 401"));
 }
-    @Test
+   @Test
     public void loginWrongPassword(){
-        app.getUser().openLoginRegistration();
+
         String email="vasiatal@gmail.com";
         String password ="Vasia34";
+        app.getUser().openLoginRegistration();
         app.getUser().fillLoginRegistrationForm(email,password);
         app.getUser().submitLogin();
 
